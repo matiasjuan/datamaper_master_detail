9e94f189368d80feec5c5db8cb65a51c0384dd56
matiasjuan <matias.juan@gmail.com>
2013-10-18 16:53:05 -0300

    added mapping Collection<Map> to xml

---
+++ src/test/resources/example2.groovy
@@ -0,0 +1,28 @@
+return [
+    [
+        patientid: 2 , 
+        values: 
+        [
+
+            [
+                "patientid": 2,
+                "lab_name": "2lab1",
+                "date": "2013/04/20",
+                "result": "ok"
+            ],
+            [
+                "patientid": 2,
+                "lab_name": "2lab2",
+                "date": "2013/04/22",
+                "result": "failed"
+            ],
+            [
+                "patientid": 2,
+                "lab_name": "2lab3",
+                "date": "2013/04/25",
+                "result": "unknown"
+            ]
+          
+        ]
+    ]
+]
